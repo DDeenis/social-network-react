3b7bb5faa4d297d3d4287dec140a1343cb000809
DDeenis <ddeenis46@gmail.com>
2021-03-29 21:16:16 +0300

    now can add message and post

--- src/components/Dialogs/Chat/Chat.js
+++ src/components/Dialogs/Chat/Chat.js
@@ -3,9 +3,25 @@ import ChatMessage from '../ChatMessage/ChatMessage'
 import styles from './Chat.module.css'
 
 function Chat(props) {
+    const newMessageArea = React.createRef();
+
+    const chatMessagesList = props.messages.map(m => <ChatMessage userName={m.name} content={m.message} key={m.name + m.name[0]} />);
+
+    const createMessage = () => {
+        const messageText = newMessageArea.current.value;
+
+        props.messages.push({ name: 'Me', message: messageText });
+
+        newMessageArea.current.value = '';
+    };
+
     return (
         <div className={styles.chat}>
-            { props.messages.map(m => <ChatMessage userName={m.name} content={m.message} key={m.name + m.name[0]} />) }
+            <div className={styles.chatMessages}>{chatMessagesList}</div>
+            <div className={styles.chatInput}>
+                <textarea className={styles.messageInput} placeholder="Type your message" ref={newMessageArea}></textarea>
+                <button className={`btn ${styles.newMessageBtn}`} onClick={createMessage}>Send</button>
+            </div>
         </div>
     );
 }
--- src/components/Profile/Posts/Posts.js
+++ src/components/Profile/Posts/Posts.js
@@ -2,17 +2,49 @@ import React from "react";
 import Post from "../Post/Post";
 import styles from './Posts.module.css';
 
+/*
+    // нормальный вариант
+
+    const [posts, changePosts] = useState(props.posts);
+
+    const maxCharacters = 70;
+    const createPost = () => {
+        const inputArea = document.querySelector(`.${styles.postInput}`);
+
+        const postText = inputArea.value.length <= maxCharacters ? inputArea.value : inputArea.value.slice(0, maxCharacters);
+
+        changePosts(posts.concat([{ content: postText, likes: 0, views: 0 }]));
+
+        inputArea.value = '';
+    };
+*/
+
 function Posts(props) {
+    const newPostArea = React.createRef();
+
+    const createPost = () => {
+        const postText = newPostArea.current.value;
+
+        // will not re-render element!!!
+        props.posts.push({ content: postText, likes: 0, views: 0 });
+
+        newPostArea.current.value = '';
+    };
+
+    const postsList = props.posts.map(
+        post => <Post text={post.content} likes={post.likes} views={post.views} key={post[0] + post[post.length - 1]} />
+    );
+
     return (
         <div className={styles.postsBlock}>
             <div className={styles.newPost}>
                 <h2>My posts</h2>
-                <textarea className={styles.postInput} placeholder="your news..."></textarea>
-                <button className={`btn ${styles.newPostBtn}`}>Send</button>
+                <textarea className={styles.postInput} placeholder="your news..." ref={newPostArea}></textarea>
+                <button className={`btn ${styles.newPostBtn}`} onClick={createPost}>Send</button>
             </div>
 
             <div className={styles.userPosts}>
-                { props.posts.map(post => <Post text={post.content} likes={post.likes} views={post.views} key={post[0] + post[post.length - 1]} />) }
+                { postsList }
             </div>
         </div>
     );
